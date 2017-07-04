71acaf08cbad60b9f5565efe3fd1521e307c42aa
George Crary <craryg@onid.oregonstate.edu>
2017-07-03 17:44:38 -0700

    Add initial select star sql and resource for getByOSUID
    CO-831

--- src/main/groovy/edu/oregonstate/mist/mealcard/db/MealPlanDAO.groovy
+++ src/main/groovy/edu/oregonstate/mist/mealcard/db/MealPlanDAO.groovy
@@ -1,10 +1,20 @@
 package edu.oregonstate.mist.mealcard.db
+
+import org.skife.jdbi.v2.sqlobject.Bind
 import org.skife.jdbi.v2.sqlobject.SqlQuery
 
-public interface MealPlanDAO {
+public interface MealPlanDAO extends Closeable {
+
+    @SqlQuery("SELECT 1 FROM dual")
+    Integer checkHealth()
 
     @SqlQuery("""
-
+                SELECT *
+                FROM mealplans
+                WHERE OSU_ID = :osuid
                 """)
-    public mealPlanByOSUID(int osuid)
+    public mealPlanByOSUID(@Bind("osuid") String osuid)
+
+    @Override
+    void close()
 }
--- src/main/groovy/edu/oregonstate/mist/mealcard/resources/MealPlanResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/mealcard/resources/MealPlanResource.groovy
@@ -1,8 +1,20 @@
 package edu.oregonstate.mist.mealcard.resources
 
 import edu.oregonstate.mist.api.Resource
+import edu.oregonstate.mist.api.jsonapi.ResourceObject
 import edu.oregonstate.mist.mealcard.db.MealPlanDAO
 
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.core.Context
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.UriInfo
+import javax.xml.ws.Response
+
+@Path("diningbalances")
+@Produces(MediaType.APPLICATION_JSON)
 class MealPlanResource extends Resource {
 
     private MealPlanDAO mealPlanDAO
@@ -11,5 +23,16 @@ class MealPlanResource extends Resource {
     MealPlanResource(MealPlanDAO dao, URI endpointUri) {
         this.mealPlanDAO = dao
         this.endpointUri = endpointUri
+    }
+
+    @GET
+    @Path('{id: [0-9]+}/')
+    Response getByOSUID(@PathParam('id') String id) {
+        System.out.println("***DEBUG***")
+        ResourceObject balances = null
+
+        def plans = mealPlanDAO.mealPlanByOSUID(id)
+
+        balances
     }
 }
